690f7f8186ba126fd185bf4854dc564749fc23d7
Felipe Barros <felipetech@tuta.io>
2020-07-21 11:01:52 -0300

    Remover console.log desnecessario

--- src/app/routes/usuarios.js
+++ src/app/routes/usuarios.js
@@ -3,7 +3,6 @@ const router = require('express').Router()
 module.exports = function (app) {
   const { usersController } = app.controllers
 
-  console.log(usersController)
   router.get('/cadastrar', usersController.registrationPage)
   router.post('/cadastrar', usersController.register)
 
